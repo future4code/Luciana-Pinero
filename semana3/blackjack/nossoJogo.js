8aa65f6f6fad91a392b13db320a2e08ae9f739ef
Luciana Verdinelli <luverdinelli@gmail.com>
2020-10-02 15:48:34 -0300

    Projeto Blackjack

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,22 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+ alert ("Bem vindo ao jogo de BlackJack!")
+ console.log ("Bem vindo ao jogo de BlackJack!")
+ 
+ if(confirm("Quer iniciar uma nova rodada?")) {
+	console.log("comprar uma carta")
+} else{
+	console.log ("O jogo acabou")
+}
+
