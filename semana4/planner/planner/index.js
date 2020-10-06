f8dd8e407c39da5964f87a54ace23de123c6db5a
Luciana Verdinelli <luverdinelli@gmail.com>
2020-10-06 17:04:30 -0300

    Planner

---
+++ semana4/planner/planner/index.js
@@ -0,0 +1,3 @@
+// A sua tarefa de hoje é criar um planner semanal digital, em que o usuário digita uma tarefa, 
+// seleciona um dia da semana em uma lista de seleção e, ao clicar em um botão, a tarefa é criada 
+// no dia da semana correto.
