769c65bba1f2a794fd26bf7cb54c0aee1db874e5
Luciana Verdinelli <luverdinelli@gmail.com>
2020-10-09 14:48:34 -0300

    Exercicio 2 - Funcao Investe Dinheiro

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -10,10 +10,22 @@
 // Como Faz: Quando a função é invocada o parâmetro incluso do valor em dolár, busca o input incluso pelo usuário através do Prompt do valor da 
 // cotação do dia, e a função fará o cálculo do valor de dolar * o valor da cotação informada pelo usuario. 
 // 
-// Valor impresso no console: O retorno no console.log será de o valor em dolar no caso 100 * X ( a cotação do dolár inclusa pelo usuario no prompt.)
+// Valor impresso no console: O retorno no console.log será de o valor em dolar no caso 100 * X ( a cotação do dolár inclusa pelo usuario no 
+// prompt.)
 
 
 // Exercicio 2:
 
-// No console.log (novoMontante) será impresso o valor de ações para o montante de 150. No caso como a função investeDinheiro 
-// informa que ações será o valor aplicado * 1.1 o valor total impresso será de 165.
+// O que ela faz: A função investeDinheiro calcula qual será o resultado dos valores aplicados (valor) em cada tipo de investimento 
+// (tipoDeInvestimento).
+
+
+// Como faz: Quando a função é invocada, é informado o valor e o tipo de investimento escolhido. Assim é percorrido todos os tipos através 
+// do Loop Switch que encontra as caracteristicas de rentabilidade de cada investimento ( fator de multiplicação), imprimindo no console o 
+// valor vezes a rentabilidade escolhida do investimento.
+
+// Valor impresso no console: No console.log (novoMontante) será impresso o valor de ações para o montante de 150. No caso como a função 
+// investeDinheiro informa que o fator de multiplicação de ações será o valor de 1.1, o valor total impresso será de 165. 
+// No console.log (segundoMontante) erá impresso o valor de tesouro Direto para o montante de 200. No caso como a função 
+// investeDinheiro não informa o fator de multiplicação de tesouro Direto a mensagem impressa será "Tipo de Investimento informado incorreto"
+
