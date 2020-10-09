166cc5c019824a842607d641b6e5057c5a2dc73c
Luciana Verdinelli <luverdinelli@gmail.com>
2020-10-09 14:33:17 -0300

    Exercicio 1 - conversão de dolár

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,19 @@
+// Lista de Exercícios:
+
+// Exercícios de interpretação de código
+
+// Exercicio 1:
+
+// O que ela faz: A função foi criada para que o usuario inclua um valor de cotação do dolár do dia e a partir daí possa incluir qualquer valor 
+// em dolár e calcular quanto seria em Reais.  
+
+// Como Faz: Quando a função é invocada o parâmetro incluso do valor em dolár, busca o input incluso pelo usuário através do Prompt do valor da 
+// cotação do dia, e a função fará o cálculo do valor de dolar * o valor da cotação informada pelo usuario. 
+// 
+// Valor impresso no console: O retorno no console.log será de o valor em dolar no caso 100 * X ( a cotação do dolár inclusa pelo usuario no prompt.)
+
+
+// Exercicio 2:
+
+// No console.log (novoMontante) será impresso o valor de ações para o montante de 150. No caso como a função investeDinheiro 
+// informa que ações será o valor aplicado * 1.1 o valor total impresso será de 165.
